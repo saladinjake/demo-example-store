10ca9f595443ed300aaf06d0d1b9d8715c0fb052
saladinjake <juwavictor@gmail.com>
2025-04-08 22:42:44 +0100

    minimalistic mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useMode } from './style/ModeProvider';
 import styled from "styled-components"
 
@@ -10,15 +11,15 @@ const Button = styled.button`
   border: none;
   cursor: pointer;
   height:40px;
-  width:300px;
+  width:200px;
 `
 
 const Container = styled.main`
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  // justify-content: center;
+  // align-items: center;
   flex-direction: column;
   background: ${props => props.theme.color.primary};
   padding:10px;
@@ -31,26 +32,57 @@ const Title = styled.h1`
   color: ${props => props.theme.color.text};
 `
 
-const Input = styled.input`
+const Input = styled.input<{width?: string}>`
  padding: 10px;
+ width: ${({width})=> width ? width :"333px"}
 `
 
-const Flex = styled.div<{ direction?: string; align?: string; width?: string; height?: string; justifyContent?: string }>`
+const Flex = styled.div<{ direction?: string;
+   align?: string;
+   width?: string; height?: string; justifyContent?: string; gap?: string }>`
   display: flex;
   width: ${({ width }) => width ? width : "100vw"};
   height: ${({ height }) => height ? height : "100%"};
   justify-content: ${({ justifyContent }) => justifyContent ? justifyContent : "center"};
   align-items: ${({ align }) => align ? align : "left"};
   flex-direction: ${({ direction }) => direction ? direction : "row"};
+  flex-gap: ${({ gap}) => gap ? gap : "10px"}
 `
 
 
 function App() {
   const [mode, setMode] = useMode();
+  const [searchField, setSearchField] = useState<string>("");
+  const [profile, setProfile] = useState<any>({});
+  const [error, setError] = useState<string>("")
   const handleToggle = () => {
     setMode((mode) => mode === 'light' ? 'dark' : 'light');
   }
 
+
+  const  handleGetApiRequest = async () => {
+    try{
+
+    
+       const response = await( await fetch(`https://api.github.com/users/${searchField}`));
+       console.log( await response.json(),">>>")
+
+       if(response.ok){
+         setProfile(response.json())
+       }else{
+         setProfile({})
+         setError("No record Found.")
+       }
+
+      } catch(error){
+        setError("Some Error Occured.")
+      }
+  }
+
+  const handleChange = (event: any) => {
+    const val  =  event.target.value as string
+    setSearchField(val)
+  }
   return (
 
     <Container>
@@ -58,14 +90,18 @@ function App() {
 
 
 
-      <Flex width="100%" height="500px" direction="row" justifyContent='space-between'>
+      <Flex width="100%" height="100px" direction="row" justifyContent='space-between'>
         <Title>Github users</Title>
 
         <Button onClick={handleToggle}>Switch To {mode.toUpperCase()} Theme</Button>
       </Flex>
+<hr/>
+        <Flex direction="row" height="100px" width="400px"  justifyContent="center" align="center">
 
-
-      
+        
+        <Input onChange={handleChange} />
+        <Button onClick={handleGetApiRequest}> Search</Button>
+      </Flex>
     </Container>
   )
 }
