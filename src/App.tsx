74f1c1dc5d7fc71830c647d57a83fca10d3929b3
saladinjake <juwavictor@gmail.com>
2025-04-08 23:57:03 +0100

    chore: simple css design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useMode } from './style/ModeProvider';
 import styled from "styled-components"
 
+
 const Button = styled.button`
   background: ${props => props.theme.color.secondary};
   color: ${props => props.theme.color.text};
@@ -16,7 +17,7 @@ const Button = styled.button`
 
 const Container = styled.main`
   width: 100%;
-  height: 100vh;
+  height: auto;
   display: flex;
   // justify-content: center;
   // align-items: center;
@@ -24,6 +25,11 @@ const Container = styled.main`
   background: ${props => props.theme.color.primary};
   padding:10px;
   margin:10px;
+
+  & h4{
+   font-size: 20px;
+   padding:10px;
+  }
 `
 
 const Title = styled.h1`
@@ -32,31 +38,42 @@ const Title = styled.h1`
   color: ${props => props.theme.color.text};
 `
 
-const Input = styled.input<{width?: string}>`
+const Input = styled.input<{ width?: string }>`
  padding: 10px;
- width: ${({width})=> width ? width :"333px"}
+ width: ${({ width }) => width ? width : "333px"}
 `
 
-const Flex = styled.div<{ direction?: string;
-   align?: string;
-   width?: string; height?: string; justifyContent?: string; gap?: string }>`
+const Flex = styled.div<{
+  direction?: string;
+  align?: string;
+  width?: string; height?: string; justifyContent?: string; gap?: string
+}>`
   display: flex;
   width: ${({ width }) => width ? width : "100vw"};
   height: ${({ height }) => height ? height : "100%"};
   justify-content: ${({ justifyContent }) => justifyContent ? justifyContent : "center"};
   align-items: ${({ align }) => align ? align : "left"};
   flex-direction: ${({ direction }) => direction ? direction : "row"};
-  flex-gap: ${({ gap}) => gap ? gap : "10px"}
+  flex-gap: ${({ gap }) => gap ? gap : "10px"}
 `
 
-const Text = styled.div`
+const Text = styled.div<{ mode?: string }>`
  font-size: 14px;
  padding: 2px;
  margin: 2px;
+ color:${({ mode }) => mode == "dark" ? "#fff" : "#888"}
 
 `
 
 const CardSample = styled.div`
+  box-shadow: 12px -50px 50px rgba(255,255,255,0.8);
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding: 12px;
+  margin: 10px;
+  border: 5px solid #fafafa;
+  border-radius: 10px;
 `
 
 const Avatar = styled.img`
@@ -67,85 +84,135 @@ const Avatar = styled.img`
  
 `
 
+const RepoContainer = styled.div`
+ padding: 10px;
+ display: flex;
+ flex-direction: row;
+  flex-gap:15px;
+width:100%;
+`
+const Repository = styled.div`
+  display: flex;
+  justify-content:center;
+ padding:20px;
+ flex-direction: column;
+
+
+`
+
+const Heading = styled.h3`
+ font-size: 20px;
+ padding : 10px;
+ margin-left:20px
+`
 function App() {
   const [mode, setMode] = useMode();
   const [searchField, setSearchField] = useState<string>("");
-  const [profile, setProfile] = useState<any>({});
-  const [error, setError] = useState<string| null>(null)
-  const [totalFollowers,setTotalFollowersCount] = useState<number>(0);
+  const [profile, setProfile] = useState<any | null>(null);
+  const [error, setError] = useState<string | null>(null)
+  const [totalFollowers, setTotalFollowersCount] = useState<number>(0);
+  const [portfolio, setPortfolios] = useState([])
+
   const handleToggle = () => {
     setMode((mode) => mode === 'light' ? 'dark' : 'light');
   }
 
 
-  const  handleGetApiRequest = async () => {
-    try{
+  const handleGetApiRequest = async () => {
+    try {
 
-    
-       const response = ( await fetch(`https://api.github.com/users/${searchField}`));
+
+      const response = (await fetch(`https://api.github.com/users/${searchField}`));
       const data = await response.json()
+      //get followers
+      const folowersResponse = await fetch(`https://api.github.com/users/${data.login}/followers`)
+      const followersResult = await folowersResponse.json()
 
-       //get followers
-       const folowersResponse = await fetch(`https://api.github.com/users/${data.login}/followers`)
-
-       const followersResult = await folowersResponse.json()
-       console.log(followersResult,">>>")
-
-       if(response.ok && folowersResponse.ok){
-         setProfile(data)
-         setTotalFollowersCount(followersResult.length)
-       }else{
-         setProfile({})
-         setError("No record Found.")
-       }
-
-      } catch(error){
-        // setError("Some Error Occured.")
+      const repositoryResponse = await fetch(`https://api.github.com/users/${data.login}/repos`)
+      const currentRepos = await repositoryResponse.json()
+      console.log(currentRepos, ">>>", data)
+      if (response.ok && folowersResponse.ok && repositoryResponse.ok) {
+        setProfile(data)
+        setTotalFollowersCount(followersResult.length)
+        setPortfolios(currentRepos)
+      } else {
+        setProfile({})
+        setError("No record Found.")
       }
 
-     
+    } catch (error) {
+      // setError("Some Error Occured.")
+    }
+
+
   }
 
   const handleChange = (event: any) => {
-    const val  =  event.target.value as string
+    const val = event.target.value as string
     setSearchField(val)
   }
   return (
-
-    <Container>
-
-
-
-
-      <Flex width="100%" height="100px" direction="row" justifyContent='space-between'>
-        <Title>Github users</Title>
-
-        <Button onClick={handleToggle}>Switch To {mode.toUpperCase()} Theme</Button>
-      </Flex>
-<hr/>
-        <Flex direction="row" height="100px" width="400px"  justifyContent="center" align="center">
-
-        
-        <Input onChange={handleChange} />
-        <Button onClick={handleGetApiRequest}> Search</Button>
-      </Flex>
+    <div>
+      <Container>
 
 
 
-      {
-        error ? <Text>{ error} </Text> : (
+
+        <Flex width="100%" height="100px" direction="row" justifyContent='space-between'>
+          <Title>Github users</Title>
+
+          <Button onClick={handleToggle}>Switch To {mode.toUpperCase()} Theme</Button>
+        </Flex>
+        <hr />
+        <Flex direction="row" height="100px" width="400px" justifyContent="center" align="center">
+
+
+          <Input onChange={handleChange} />
+          <Button onClick={handleGetApiRequest}> Search</Button>
+        </Flex>
+
+
+        <CardSample>      {
+          error ? <Text>{error} </Text> : (
+            <>
+              {profile && (
+                <div>
+                  <Avatar src={profile?.avatar_url} />
+                  <Text>Name: {profile?.name}</Text>
+
+                  <Text>Followers (count): {totalFollowers} </Text>
+                  <Text>Github profile: <a style={{ cursor: "pointer", color: "blue" }} href={profile.url} target='blank' >Here</a> </Text>
+                </div>
+              )}
+            </>
+          )
+        }
+        </CardSample>
+
+      </Container>
+
+
+
+      <Heading> Github Repos</Heading>
+      <CardSample>
+      
+
+        {portfolio && (
           <>
-          {profile && (
-            <div>
-               <Avatar src={profile?.avatar_url} />
-               <Text>Name: {profile?.login}</Text>
-               <Text>Followers (count): {totalFollowers} </Text>
-            </div>
-          )}
+            {portfolio?.map((repo: any) => {
+              return (
+                <Repository>
+                  <Text> <a href={repo.content_url} target="_blank"> Name: {repo?.name}</a></Text>
+                  {repo.description && (<Text>Description: {repo.description}</Text>)}
+                  <Text>Forks Count: {repo.forks_count}</Text>
+                  <Text>Star Count:{repo.stargazers_count}</Text>
+                </Repository>
+              )
+            })}
           </>
-        )
-      }
-    </Container>
+        )}
+      </CardSample>
+    </div>
   )
 }
 
