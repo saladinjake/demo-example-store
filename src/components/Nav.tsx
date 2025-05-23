3714e060185ecf980331313df2022be86e792d9a
saladinjake <juwavictor@gmail.com>
2025-05-23 22:55:31 +0100

    chore: add header component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components"
+export const Header = () => {
+
+    return (
+
+     <HeaderWraper>
+        <a href="#"><img src="images/logo.png" className="logo" alt="Shop" /></a>
+        <div>
+            <Menu>
+                <MenuItem><a className="active" href="/">Home</a></MenuItem>
+                <MenuItem><a href="/shop">Shop</a></MenuItem>
+                <MenuItem><a href="/poMenuItemcy">Blog</a></MenuItem>
+                <MenuItem><a href="/login">About</a></MenuItem>
+                <MenuItem><a href="/signup">Contact</a></MenuItem>
+                <MenuItem id="lg-bag"><a href="/cart"><i className="far fa-shopping-bag"></i></a></MenuItem>
+                <a href="#" id="close"><i className="far fa-times"></i></a>
+            </Menu>
+        </div>
+        <div id="mobile">
+            <a href="/cart"><i className="far fa-shopping-bag"></i></a>
+            <i id="bar" className="fas fa-outdent"></i>
+        </div>
+    </HeaderWraper>
+    )
+}
+
+
+const HeaderWraper = styled.section`
+
+    display: flex;
+    aMenuItemgn-items: center;
+    justify-content: space-between;
+    padding: 20px 80px;
+    background: #E3E6F3;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.06);
+    z-index: 999;
+    position: sticky;
+    top: 0;
+
+
+#close {
+    display: none;
+}
+
+#mobile {
+    display: none;
+    aMenuItemgn-items: center;
+}
+
+`
+
+
+const Menu =styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const MenuItem =styled.li`
+
+&   {
+    list-style: none;
+    padding: 0 20px;
+    position: relative;
+}
+
+&   a {
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: 600;
+    color: #1a1a1a;
+    transition: 0.3s ease; 
+}
+
+&   a:hover,
+&   a.active {
+    color: #088178;
+}
+
+&   a:hover::after,
+&   a.active::after {
+    content: "";
+    width: 30%;
+    height: 2px;
+    background: #088178;
+    position: absolute;
+    bottom: -4px;
+    left: 20px;
+}
+`
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
-
+import { Header } from "../components/Nav"
 export const RouteOutlets = () => {
 
     return (
+        <>
+          <Header />
          <Routes>
           <Route exact path="/" element={<>Home</>} />
           <Route path="/products" element={<>Products page</>} />
           <Route path="/cart" element={<>Cart page</>} />
           <Route path="/login" element={<>Login</>} />
         </Routes>
+
+        </>
     )
 }
