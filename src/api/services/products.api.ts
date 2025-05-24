87805a38da3e590fb4412c990d411269b9f850dd
saladinjake <juwavictor@gmail.com>
2025-05-24 22:50:43 +0100

    chore: setup axios config

---
+++ src/api/services/products.api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const LOCAL_STORAGE_KEY = "products";
+
+export async function fetchProducts() {
+  const stored = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (stored) {
+    return JSON.parse(stored);
+  }
+
+  const res = await axios.get("https://fakestoreapi.com/products");
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(res.data));
+  return res.data;
+}
