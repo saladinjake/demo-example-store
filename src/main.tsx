b90c408189aab2551c28305d9e5c48d14ac6eb50
saladinjake <juwavictor@gmail.com>
2025-06-05 22:21:23 +0100

    chore: responsiv login

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { ModeProvider } from './theme/style/ModeProvider.tsx'
 import Theme from './theme';
 import App from './App.tsx'
-import { GlobalStyles  } from './theme/globalStyles';
+import { GlobalStyles } from './theme/globalStyles';
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./contexts/AuthContext"
-
+import Loader from './components/UIElements/Loader/Loader.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ModeProvider>
       <Theme>
-         <BrowserRouter>
-         <AuthProvider>
-         
-             <App />
-         
-        </AuthProvider>
+        <BrowserRouter>
+          <Suspense fallback={<Loader variant={"sm"} />}>
+            <AuthProvider>
+
+              <App />
+
+            </AuthProvider>
+          </Suspense>
         </BrowserRouter>
         <GlobalStyles />
       </Theme>
     </ModeProvider>
-    
+
   </React.StrictMode>,
 )
